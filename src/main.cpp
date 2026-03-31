d826d9ec0861da414e5cae861a8a55b9f95e8abe
Karlo <karlotabios@gmail.com>
2026-03-31 22:41:19 +0800

    Added FPS measurement

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,28 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
+
+#define FPS 60.0f
+#define TIMESTEP (1.0f / FPS)
 
 int main()
 {
+	// Window objects
 	sf::RenderWindow window( sf::VideoMode( { 200, 200 } ), "SFML works!" );
 	sf::CircleShape shape( 100.f );
 	shape.setFillColor( sf::Color::Green );
 
+	// Time tracking objects
+	sf::Clock clock;
+	sf::Time elapsedTime;
+	sf::Time iterationTime;
+
 	while ( window.isOpen() )
 	{
+		std::cout << elapsedTime.asMilliseconds() << "\n";
+		elapsedTime = sf::seconds(0);
+		iterationTime = clock.restart();
+		elapsedTime += iterationTime;
+
 		while ( const std::optional event = window.pollEvent() )
 		{
 			if ( event->is<sf::Event::Closed>() )
@@ -17,5 +32,11 @@ int main()
 		window.clear();
 		window.draw( shape );
 		window.display();
+
+		sf::Time sleepTime = sf::seconds(TIMESTEP) - clock.restart();
+		if (sleepTime.asSeconds() > 0)
+		{
+			sf::sleep(sleepTime);
+		}
 	}
 }
