d1e667e8031a368a4f5533c905b72b9a6dde0b2a
Karlo <karlotabios@gmail.com>
2026-04-02 19:46:50 +0800

    Added namespace to Defaults

--- src/defaults/defaults.h
+++ src/defaults/defaults.h
@@ -1,7 +1,9 @@
 #pragma once
 
-static const float FPS = 60.0f;
-static const float TIMESTEP = (1.0f / FPS);
-static const float WINDOW_HEIGHT = 600.0f;
-static const float WINDOW_WIDTH = 800.0f;
-static const std::string FONT_DIR = "../font/Roboto/static/Roboto_Condensed-Black.ttf";
+namespace kt::Defaults {
+	static const float FPS = 60.0f;
+	static const float TIMESTEP = (1.0f / FPS);
+	static const float WINDOW_HEIGHT = 600.0f;
+	static const float WINDOW_WIDTH = 800.0f;
+	static const std::string FONT_DIR = "../font/Roboto/static/Roboto_Condensed-Black.ttf";
+}
