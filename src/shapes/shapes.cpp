b6fa8da69d9cfb4fccdd91223659828cfd858edb
Karlo <karlotabios@gmail.com>
2026-04-02 19:46:34 +0800

    Added my own CircleEntity class, inheriting CircleShape. Intended to be a wrapper for an object that has acceleration and velocity

--- src/shapes/shapes.cpp
+++ src/shapes/shapes.cpp
@@ -1,2 +1,30 @@
-#include "shapes.h"
+#include "Shapes.h"
 
+namespace kt::Shapes {
+	CircleEntity::CircleEntity() : CircleShape() {
+		CircleEntity::setRadius(30.0f);
+		CircleEntity::setFillColor(sf::Color::Red);
+		CircleEntity::setOutlineColor(sf::Color::Red);
+	}
+
+	sf::Vector2f CircleEntity::getAcceleration() {
+		return acceleration;
+	}
+	void CircleEntity::setAcceleration(const sf::Vector2f& accel) {
+		acceleration = accel;
+	}
+
+	sf::Vector2f CircleEntity::getVelocity() {
+		return velocity;
+	}
+	void CircleEntity::setVelocity(const sf::Vector2f& vel) {
+		velocity = vel;
+	}
+
+	float CircleEntity::getMass() {
+		return mass;
+	}
+	void CircleEntity::setMass(const float& m) {
+		mass = m;
+	}
+}
--- src/shapes/shapes.h
+++ src/shapes/shapes.h
@@ -1,1 +1,26 @@
 #pragma once
+
+#include <SFML/Graphics.hpp>
+
+namespace kt::Shapes {
+	class CircleEntity : public sf::CircleShape	{
+	private:
+		sf::Vector2f acceleration{ 0.0f, 0.0f };
+		sf::Vector2f velocity{ 0.0f, 0.0f };
+		float mass = 1;
+
+	public:
+		using CircleShape::CircleShape;
+
+		CircleEntity();
+
+		sf::Vector2f getAcceleration();
+		void setAcceleration(const sf::Vector2f& accel);
+
+		sf::Vector2f getVelocity();
+		void setVelocity(const sf::Vector2f& vel);
+
+		float getMass();
+		void setMass(const float& m);
+	};
+}
