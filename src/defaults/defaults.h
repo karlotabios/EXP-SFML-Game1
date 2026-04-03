a7c5789dedeb9499a1078794adee3e3f7b72eef7
Karlo <karlotabios@gmail.com>
2026-04-03 19:48:28 +0800

    Change view window dimensions

--- src/defaults/defaults.h
+++ src/defaults/defaults.h
@@ -5,8 +5,8 @@
 namespace kt::Defaults {
 	static const float FPS = 60.0f;
 	static const float TIMESTEP = (1.0f / FPS);
-	static const float WINDOW_HEIGHT = 600.0f;
-	static const float WINDOW_WIDTH = 800.0f;
+	static const float WINDOW_HEIGHT = 1040.0f;
+	static const float WINDOW_WIDTH = 1920.0f;
 	static const std::string FONT_DIR = "../font/Roboto/Roboto-VariableFont_wdth,wght.ttf";
 	/*static const std::string FONT_DIR = "../font/Roboto/static/Roboto_Condensed-Black.ttf";*/
 }
