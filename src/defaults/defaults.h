973c92885f5e79ab38950c3c1c27177405e146ea
Karlo <karlotabios@gmail.com>
2026-04-03 19:25:14 +0800

    Changed FONT_DIR

--- src/defaults/defaults.h
+++ src/defaults/defaults.h
@@ -1,9 +1,12 @@
 #pragma once
 
+#include <SFML/Graphics.hpp>
+
 namespace kt::Defaults {
 	static const float FPS = 60.0f;
 	static const float TIMESTEP = (1.0f / FPS);
 	static const float WINDOW_HEIGHT = 600.0f;
 	static const float WINDOW_WIDTH = 800.0f;
-	static const std::string FONT_DIR = "../font/Roboto/static/Roboto_Condensed-Black.ttf";
+	static const std::string FONT_DIR = "../font/Roboto/Roboto-VariableFont_wdth,wght.ttf";
+	/*static const std::string FONT_DIR = "../font/Roboto/static/Roboto_Condensed-Black.ttf";*/
 }
