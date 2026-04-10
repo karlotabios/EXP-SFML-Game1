5405ab56c8ef2f17aa4176b052d469cdf9e2a011
Karlo <karlotabios@gmail.com>
2026-04-11 01:20:31 +0800

    Added default values for friction, player movement force, and anti-aliasing.

--- src/defaults/defaults.h
+++ src/defaults/defaults.h
@@ -2,11 +2,18 @@
 
 #include <SFML/Graphics.hpp>
 
+// std::clamp
+#include <algorithm>
+
 namespace kt::Defaults {
+	static const unsigned int ANTI_ALIASING_LEVEL = 8;
 	static const float FPS = 60.0f;
 	static const float TIMESTEP = (1.0f / FPS);
 	static const float WINDOW_HEIGHT = 1040.0f;
 	static const float WINDOW_WIDTH = 1920.0f;
 	static const std::string FONT_DIR = "../font/Roboto/Roboto-VariableFont_wdth,wght.ttf";
-	/*static const std::string FONT_DIR = "../font/Roboto/static/Roboto_Condensed-Black.ttf";*/
+	static const float STATIC_FRICTION_COEF = 0.5f;		// Values ideally clamped from 0.0f to 1.0f
+	static const float KINETIC_FRICTION_COEF = 0.3f;	// Values ideally clamped from 0.0f to 1.0f
+	static const float GRAVITY_ACCELERATION = 9.8f;
+	static const sf::Vector2f PLAYER_MOVEMENT_FORCE{ 150.0f, 150.0f };
 }
