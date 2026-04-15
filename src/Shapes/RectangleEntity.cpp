26210b07c0b161d82a07499d416c40db85df56f0
Karlo <karlotabios@gmail.com>
2026-04-15 22:13:44 +0800

    Set origin of RectangleEntity to its center.

--- src/Shapes/RectangleEntity.cpp
+++ src/Shapes/RectangleEntity.cpp
@@ -3,6 +3,7 @@
 namespace kt::Shapes {
 	RectangleEntity::RectangleEntity() : RectangleShape() {
 		setSize(sf::Vector2f{ 100.0f, 100.0f });
+		setOrigin(sf::Vector2f{ getSize().x / 2.0f, getSize().y / 2.0f });
 		setFillColor(sf::Color::Blue);
 	}
 
