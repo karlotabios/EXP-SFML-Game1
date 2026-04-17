33d8c0186ac58fdb24340d10fd451e3fb4945290
Karlo <karlotabios@gmail.com>
2026-04-17 19:36:35 +0800

    Finalized Circle vs Rectangle collision detection

--- src/Utils/Math.cpp
+++ src/Utils/Math.cpp
@@ -15,17 +15,13 @@ namespace kt::Utils {
 		nearestPoint.x = std::clamp(circle.getPosition().x, xMin, xMax);
 		nearestPoint.y = std::clamp(circle.getPosition().y, yMin, yMax);
 
-		sf::Vector2f distanceVector = nearestPoint - circle.getPosition();
-
-		std::cout << "[INFO/Collision] distanceVector.x: " << distanceVector.x << std::endl;
-		std::cout << "[INFO/Collision] distanceVector.y: " << distanceVector.y << std::endl;
+		sf::Vector2f distanceVector = circle.getPosition() - nearestPoint;
 
 		// If the distance to the circle from the nearest point within the rectangle is less than the radius of the circle, then there is collision
 		if (distanceVector.length() < circle.getRadius()) {
-			std::cout << kt::Globals::COUT_RED << "[INFO/Collision] Collision detected!!" << kt::Globals::COUT_DEFAULT << std::endl;
-
 			float overlapMagnitude = 0;
 
+			// Case 1: If circle's origin is within the area of the rectangle, then nearestPoint will be the position of the circle itself which will result in a division by zero in vector normalization (wrong). Instead get the points on the edge of the rectangle that are nearest to the circle's position, and compute the normal and magnitude of overlap from there.
 			// Check if the circle's origin is inside the rectangle
 			if (distanceVector.length() == 0) {
 				// Get all nearest points on each edge of the rectangle
@@ -34,48 +30,47 @@ namespace kt::Utils {
 				const sf::Vector2f nearestBottom{ circle.getPosition().x, yMax };
 				const sf::Vector2f nearestLeft{ xMin, circle.getPosition().y };
 
-				// Get the nearest point from each edge
-				const float minimumDistance = std::min({ nearestTop.length(), nearestRight.length(), nearestBottom.length(), nearestLeft.length() });
+				const float distanceTop = (nearestTop - circle.getPosition()).length();
+				const float distanceRight = (nearestRight - circle.getPosition()).length();
+				const float distanceBottom = (nearestBottom - circle.getPosition()).length();
+				const float distanceLeft = (nearestLeft - circle.getPosition()).length();
+
+				// Get the distance to the nearest point on the edges
+				float minimumDistance = std::min({ distanceTop, distanceRight, distanceBottom, distanceLeft });
 
 				sf::Vector2f collisionNormalDirection{};
 
-				if (minimumDistance == nearestTop.length()) {
+				if (minimumDistance == distanceTop) {
 					collisionNormalDirection = sf::Vector2f{ 0.0f, -1.0f };
 					overlapMagnitude = circle.getPosition().y - yMin + circle.getRadius();
 				}
-				if (minimumDistance == nearestRight.length()) {
+				else if (minimumDistance == distanceRight) {
 					collisionNormalDirection = sf::Vector2f{ 1.0f, 0.0f };
 					overlapMagnitude = xMax - circle.getPosition().x + circle.getRadius();
 				}
-				if (minimumDistance == nearestBottom.length()) {
+				else if (minimumDistance == distanceBottom) {
 					collisionNormalDirection = sf::Vector2f{ 0.0f, 1.0f };
 					overlapMagnitude = yMax - circle.getPosition().y + circle.getRadius();
 				}
-				if (minimumDistance == nearestLeft.length()) {
+				else if (minimumDistance == distanceLeft) {
 					collisionNormalDirection = sf::Vector2f{ -1.0f, 0.0f };
 					overlapMagnitude = circle.getPosition().x - xMin + circle.getRadius();
 				}
-
-				std::cout << "[INFO/Collision] collisionNormalDirection.x: " << collisionNormalDirection.x << std::endl;
-				std::cout << "[INFO/Collision] collisionNormalDirection.y: " << collisionNormalDirection.y << std::endl;
-				std::cout << "[INFO/Collision] overlapMagnitude: " << overlapMagnitude << std::endl;
 
 				return CollisionData{
 					collisionNormalDirection,
 					overlapMagnitude
 				};
 			}
+			// Case 2: If the circle's origin is outside the rectangle, then simply get the direction from the nearestPoint and the circle's position, and then get the magnitude to scale the direction from the overlap (from the distance of the radius and nearestPoint)
+			else {
+				overlapMagnitude = circle.getRadius() - distanceVector.length();
 
-			overlapMagnitude = circle.getRadius() - distanceVector.length();
-
-			std::cout << "[INFO/Collision] -(distanceVector.normalized()).x: " << -(distanceVector.normalized()).x << std::endl;
-			std::cout << "[INFO/Collision] -(distanceVector.normalized()).y: " << -(distanceVector.normalized()).y << std::endl;
-			std::cout << "[INFO/Collision] overlapMagnitude: " << overlapMagnitude << std::endl;
-
-			return CollisionData{
-				-(distanceVector.normalized()),
-				overlapMagnitude
-			};
+				return CollisionData{
+					distanceVector.normalized(),
+					overlapMagnitude
+				};
+			}
 		}
 			
 		return std::nullopt;
