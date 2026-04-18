ca3ba8a2488cf50ac21303ce5347e785edbb5cbf
Karlo <karlotabios@gmail.com>
2026-04-18 16:22:03 +0800

    Changed most const marked variables to constexpr

--- src/Globals/defaults.h
+++ src/Globals/defaults.h
@@ -6,15 +6,16 @@
 #include <algorithm>
 
 namespace kt::Globals {
-	static const unsigned int ANTI_ALIASING_LEVEL = 8;
-	static const float FPS = 60.0f;
-	static const float TIMESTEP = (1.0f / FPS);
-	static const float WINDOW_HEIGHT = 1040.0f;
-	static const float WINDOW_WIDTH = 1920.0f;
+	static constexpr unsigned int ANTI_ALIASING_LEVEL = 8;
+	static constexpr float FPS = 60.0f;
+	static constexpr float TIMESTEP = (1.0f / FPS);
+	static constexpr float WINDOW_HEIGHT = 1040.0f;
+	static constexpr float WINDOW_WIDTH = 1920.0f;
 	static const std::string FONT_DIR = "../font/Roboto/Roboto-VariableFont_wdth,wght.ttf";
-	static const float STATIC_FRICTION_COEF = 0.75f;		// Values ideally clamped from 0.0f to 1.0f
-	static const float KINETIC_FRICTION_COEF = 0.5f;	// Values ideally clamped from 0.0f to 1.0f
-	static const float GRAVITY_ACCELERATION = 1000.0f;
-	static const sf::Vector2f PLAYER_MOVEMENT_FORCE{ 10000.0f, 10000.0f };
-	static const float LAG_SPIKE_TIME_SECONDS = 1.0f;
+	static const std::string LAG_IMAGE_DIR = "../img/lag.png";
+	static constexpr float STATIC_FRICTION_COEF = 0.75f;		// Values ideally clamped from 0.0f to 1.0f
+	static constexpr float KINETIC_FRICTION_COEF = 0.5f;	// Values ideally clamped from 0.0f to 1.0f
+	static constexpr float GRAVITY_ACCELERATION = 1000.0f;
+	static constexpr sf::Vector2f PLAYER_MOVEMENT_FORCE{ 10000.0f, 10000.0f };
+	static constexpr float LAG_SPIKE_TIME_SECONDS = 1.0f;
 }
