f227cd5dba24faa6512614b6d30eaa68418622f4
Karlo <karlotabios@gmail.com>
2026-04-25 01:46:13 +0800

    Revert "Changed to public inheritance for TextEntity"
    This reverts commit 3d045d38d474912143fd63b0ea2c46a514acc014.

--- src/text/text.cpp
+++ src/text/text.cpp
@@ -1,7 +1,7 @@
 #include "Text.h"
 
 namespace kt::Text {
-	TextEntity::TextEntity(const sf::Font& font) : public sf::Text(font) {};
+	TextEntity::TextEntity(const sf::Font& font) : sf::Text(font) {};
 
 	sf::Vector2f TextEntity::getVelocity() const {
 		return m_velocity;
