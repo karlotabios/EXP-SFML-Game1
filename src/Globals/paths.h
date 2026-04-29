295c5bfb1610072c94a8d0a6bdf7953a2c128731
Karlo <karlotabios@gmail.com>
2026-04-29 21:23:02 +0800

    Added paths.h, containing #defines for paths for files such as "../Core/CoreSimulation.h". Not used. Potentially bad idea

---
+++ src/Globals/paths.h
@@ -0,0 +1,56 @@
+#pragma once
+
+// Core folder
+#ifndef _CoreSimulation_PATH
+#define _CoreSimulation_PATH "../Core/CoreSimulation.h"
+#endif
+
+// Globals folder
+#ifndef _Color_PATH
+#define _Color_PATH "../Globals/color.h"
+#endif
+
+#ifndef _Defaults_PATH
+#define _Defaults_PATH "../Globals/defaults.h"
+#endif
+
+// Level Folder
+#ifndef _Level_PATH
+#define _Level_PATH "../Level/Level.h"
+#endif
+
+// Physical folder
+#ifndef _Movable_PATH
+#define _Movable_PATH "../Physical/Movable.h"
+#endif
+
+// Shapes folder
+#ifndef _CircleEntity_PATH
+#define _CircleEntity_PATH "../Shapes/CircleEntity.h"
+#endif
+
+#ifndef _RectangleEntity_PATH
+#define _RectangleEntity_PATH "../Shapes/RectangleEntity.h"
+#endif
+
+// Text folder
+#ifndef _Text_PATH
+#define _Text_PATH "../Text/Text.h"
+#endif
+
+// Utils folder
+#ifndef _Frametime_PATH
+#define _Frametime_PATH "../Utils/Frametime.h"
+#endif
+
+#ifndef _Math_PATH
+#define _Math_PATH "../Utils/Math.h"
+#endif
+
+#ifndef _Print_PATH
+#define _Print_PATH "../Utils/print.h"
+#endif
+
+#ifndef _SimulationExceptions_PATH
+#define _SimulationExceptions_PATH "../Utils/SimulationExceptions.h"
+#endif
