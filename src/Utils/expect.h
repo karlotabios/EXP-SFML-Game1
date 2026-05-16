bf714f8c48619060e00e3a60d6d9f3dd5820c847
Karlo <karlotabios@gmail.com>
2026-05-16 13:33:57 +0800

    Renamed template argument for readability

--- src/Utils/expect.h
+++ src/Utils/expect.h
@@ -18,25 +18,25 @@ namespace kt::Utils {
 
 	enum class ErrorAction { ActionLog, ActionThrow, ActionTerminate };
 
-	template<ErrorAction action = ErrorAction::ActionThrow, typename P, typename T>
+	template<ErrorAction defaultThrow = ErrorAction::ActionThrow, typename P, typename T>
 	bool expect(P& predicate, const T& except) {
 		static_assert(std::is_invocable_v<decltype(predicate)>, "[STATIC_ASSERT FAILURE] Cannot invoke non-function object argument \"P& predicate\". Argument is meant to be a predicate type with call operator.");
 		static_assert(is_std_exception_v<T>, "[STATIC_ASSERT FAILURE] \"expect\" function cannot proceed with passed argument. \"const T& except\" argument must be type derived from or exactly std::exception");
 		
 		bool result{ predicate() };
 
-		if constexpr (action == ErrorAction::ActionLog) {
+		if constexpr (defaultThrow == ErrorAction::ActionLog) {
 			if (!result) {
 				kt::Utils::printError("[EXPECT FAIL] expect() encountered predicate that returned false.");
 				kt::Utils::printError(except.what());
 			}
 		}
-		if constexpr (action == ErrorAction::ActionThrow) {
+		if constexpr (defaultThrow == ErrorAction::ActionThrow) {
 			if (!result) {
 				throw except;
 			}
 		}
-		if constexpr (action == ErrorAction::ActionTerminate) {
+		if constexpr (defaultThrow == ErrorAction::ActionTerminate) {
 			if (!result) {
 				std::terminate();
 			}
